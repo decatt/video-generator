3f36459301244f990223fdf05c06f7af77afdb83
dmcatte <dmcatte1995@gmail.com>
2024-02-28 10:28:24 +0000

    edge_tts

--- tts.py
+++ tts.py
@@ -1,19 +1,18 @@
-import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voices', voices[0].id)
-vol = engine.getProperty('volume')
-engine.setProperty('volume', vol+0.5)
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate+20)
-all_text = ''
+import edge_tts
+import asyncio
+TEXT = ""
 with open("all.txt", 'r', encoding='utf-8') as f:
     lines = f.readlines()
     for line in lines:
       line = line.strip().replace('\n', ' ') 
-      all_text += line + ' '
-print(all_text)
-engine.say(all_text)
-engine.save_to_file(all_text, './all.mp3')
-engine.runAndWait()
-engine.stop()
+      TEXT += line + ' '
+print(TEXT)
+voice = 'zh-CN-YunjianNeural'
+output = 'demo.mp3'
+rate = '-4%'
+volume = '+0%'
+async def my_function():
+    tts = edge_tts.Communicate(text = TEXT,voice = voice,rate = rate,volume=volume)
+    await tts.save(output)
+if __name__ == '__main__':
+    asyncio.run(my_function())
